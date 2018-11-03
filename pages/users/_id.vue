6c2becf94f04c9b631434005ca17ada2473c1dc2
Nolit <yaboonjapan@gmail.com>
2018-11-03 14:28:27 +0900

    ユーザーページのファイルだけ作成

---
+++ pages/users/_id.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        UserId: {{ id}}
+    </div>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                id: this.$route.params.id
+            }
+        }
+    }
+</script>
