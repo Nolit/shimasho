7dc7b9478114e1f71dedc82aa994d302d94ff318
Nolit <yaboonjapan@gmail.com>
2018-12-02 12:16:49 +0900

    ダイアログの背景タッチを無効化

--- components/dialog/TaskCreation.vue
+++ components/dialog/TaskCreation.vue
@@ -1,5 +1,5 @@
 <template>
-    <v-dialog v-model="isOpen" max-width="500px">
+    <v-dialog v-model="isOpen" max-width="500px" persistent>
         <v-card>
           <v-card-text>
             <v-flex>
