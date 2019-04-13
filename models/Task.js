7578345a4227cdb931c896be2fe4b0e1eb221220
Nolit <yaboonjapan@gmail.com>
2019-04-14 02:20:58 +0900

    タスクモデル作成

---
+++ models/Task.js
@@ -0,0 +1,49 @@
+const STEP_UP = 0
+const ACHIEVE = 1
+const TIME = 2
+
+export default class Task {
+    constructor(id, title, type, dueDate, amount, progress, isAchieved) {
+        this.id = id
+        this.title = title
+        this.type = type
+        this.dueDate = dueDate
+        this.amount = amount
+        this.progress = progress
+        this.isAchievedFlag = isAchieved
+    }
+
+    isStepUp() {
+        return this.type === STEP_UP
+    }
+
+    isAchieve() {
+        return this.type === ACHIEVE
+    }
+
+    isTime() {
+        return this.type === TIME
+    }
+
+    isAchieved() {
+        if (this.isAchieve()) {
+            return this.isAchievedFlag
+        }
+        if (this.isStepUp()) {
+            return this.progress >= this.amount
+        }
+        if (this.isTime()) {
+            return false
+        }
+    }
+}
+
+function createStepUpTask(id, title, dueDate, amount, progress) {
+    return new Task(id, title, STEP_UP, dueDate, amount, progress, 0)
+}
+
+function createAchieveTask(id, title, dueDate, isAchieved) {
+    return new Task(id, title, ACHIEVE, dueDate, 0, 0, isAchieved)
+}
+
+export {STEP_UP, ACHIEVE, TIME, createStepUpTask, createAchieveTask}
