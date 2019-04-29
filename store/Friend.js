eac3b65d366768912638c8ee633a3c515db6870e
Nolit <yaboonjapan@gmail.com>
2019-04-29 12:40:57 +0900

    フォロー機能の作成

--- store/Friend.js
+++ store/Friend.js
@@ -33,18 +33,12 @@ export default {
         async fetchFollowCandidates({ state, commit, dispatch, rootState }) {
             const res = await client.get(`/users/${rootState.signInUser.id}/friend-candidates`)
             const followCandidates = res.data.map(user => new User(user.id, user.userName))
-            //ToDo: jsonのデシリアライズ
-            //ToDo: Userの配列に変換
-            const friendCandidates = [
-                new User(1, 'フレンド1'),
-                new User(2, 'フレンド2'),
-                new User(3, 'フレンド3'),
-            ]
             commit('setFollowCandidates', followCandidates)
         },
-        async follow({ state, commit, dispatch }, user) {
-            console.log("follow")
-            console.log(user)
+        async follow({ state, commit, dispatch, rootState }, user) {
+            const res = await client.put(`/users/${rootState.signInUser.id}/followed-users/${user.id}`)
+            commit('closeFollowDialog')
+            dispatch('fetchFollowedUsers')
         }
     }
 }
