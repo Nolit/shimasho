68e7b2d15a633201a6756f06dad12db6b77c9c03
Nolit <yaboonjapan@gmail.com>
2019-06-05 00:20:48 +0900

    フォローしているユーザーに関するURLを変更

--- store/Friend.js
+++ store/Friend.js
@@ -24,22 +24,22 @@ export default {
     },
     actions: {
         async fetchFollowedUsers({ state, commit, dispatch, rootState }) {
-            const res = await client.get(`/users/${rootState.signInUser.id}/followed-users`)
+            const res = await client.get(`/users/${rootState.signInUser.id}/followees`)
             const followedUsers = res.data.map(user => new User(user.id, user.userName))
             commit('setFollowedUsers', followedUsers)
         },
         async fetchFollowCandidates({ state, commit, dispatch, rootState }) {
-            const res = await client.get(`/users/${rootState.signInUser.id}/friend-candidates`)
+            const res = await client.get(`/users/${rootState.signInUser.id}/followees-candidates`)
             const followCandidates = res.data.map(user => new User(user.id, user.userName))
             commit('setFollowCandidates', followCandidates)
         },
         async follow({ commit, dispatch, rootState }, user) {
-            await client.put(`/users/${rootState.signInUser.id}/followed-users/${user.id}`)
+            await client.put(`/users/${rootState.signInUser.id}/followees/${user.id}`)
             commit('closeFollowDialog')
             dispatch('fetchFollowedUsers')
         },
         async unfollow({ commit, dispatch, rootState }, userId) {
-            await client.delete(`/users/${rootState.signInUser.id}/followed-users/${userId}`)
+            await client.delete(`/users/${rootState.signInUser.id}/followees/${userId}`)
             commit('closeFollowDialog')
             dispatch('fetchFollowedUsers')
         }
