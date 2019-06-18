a9b5dbae115de05177c0bf2ef9346abd6bd5657a
Nolit <yaboonjapan@gmail.com>
2019-06-19 02:23:14 +0900

    ナビゲーションに利用するコンポーネントの変更

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,21 +1,58 @@
 <template>
-    <v-tabs
-        centered
-        icons-and-text
-        app
-        hide-slider="true"
+    <v-bottom-nav
+        :active="navigation"
+        :color="color"
+        :value="true"
+        fixed
+        dark
+        shift
     >
-        <v-tab @click="$router.push('/tasks')" :key="'tasks'">
-            Task
+        <v-btn @click="$router.push('/tasks')" dark>
+            <span>Task</span>
             <v-icon>list</v-icon>
-        </v-tab>
-        <v-tab active @click="$router.push('/timelines')" :key="'timeline'">
-            Timeline
-            <v-icon>timeline</v-icon>
-        </v-tab>
-        <v-tab @click="$router.push('/friends')" :key="'friends'">
-            Friend
+        </v-btn>
+
+        <v-btn @click="$router.push('/timelines')" dark>
+            <span>Timeline</span>
+            <v-icon>timelines</v-icon>
+        </v-btn>
+
+        <v-btn @click="$router.push('/friends')" dark>
+            <span>Friend</span>
             <v-icon>group</v-icon>
-        </v-tab>
-    </v-tabs>
+        </v-btn>
+    </v-bottom-nav>
 </template>
+
+<script>
+import { mapMutations, mapState, mapActions, mapGetters   } from 'vuex'
+export default {
+    created() {
+        const route = this.$router.currentRoute.name
+        if (route === 'tasks') {
+           this.navigation = 0
+        }
+        if (route === 'timelines') {
+           this.navigation = 1
+        }
+        if (route === 'friends') {
+           this.navigation = 2
+        }
+    },
+    data () {
+        return {
+            navigation: null
+        }
+    },
+    computed: {
+        color () {
+            switch (this.navigation) {
+                case 0: return 'blue-grey'
+                case 1: return 'teal'
+                case 2: return 'brown'
+                case 3: return 'indigo'
+            }
+        }
+    }
+}
+</script>
